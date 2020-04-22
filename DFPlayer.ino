0faf5edb6400b176743b857f5fd989817dfe4e70
hshshv <53831820+hshshv@users.noreply.github.com>
2020-04-22 11:11:10 +0300

    Create DFPlayer.ino

---
+++ DFPlayer.ino
@@ -0,0 +1,28 @@
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"//you need to download this
+
+#define TX 2
+#define RX 3
+
+SoftwareSerial SOS(RX, TX);
+DFRobotDFPlayerMini Player;
+
+void setup() 
+{
+  Serial.begin(9600);//לא נחוץ
+  SOS.begin(9600);
+  if(Player.begin(SOS))//הצליח להתחיל את הנגן
+  {
+    Serial.println("it is fine");
+  }
+  else
+  {
+    Serial.println("XXXXX");
+  }
+  Player.volume(30); // 0 - 30
+}
+
+void loop() 
+{
+  Player.play(0);//the first file you copied to the card.
+}
